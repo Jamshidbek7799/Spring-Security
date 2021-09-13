51cc2dc71bf7e4d65d7ad7c7ab91660b533aa0c5
Jamshid <jamshidyuldashev7799@gmail.com>
2021-09-13 09:48:23 +0500

    add

---
+++ src/main/java/com/example/demo/model/Permission.java
@@ -0,0 +1,17 @@
+package com.example.demo.model;
+
+public enum Permission {
+    DEVELOPERS_READ("developers:read"),
+    DEVELOPERS_WRITE("developers:write");
+
+    private String permission;
+
+    Permission(String permission){
+        this.permission = permission;
+    }
+
+    public String getPermission(){
+        return permission;
+    }
+
+}
