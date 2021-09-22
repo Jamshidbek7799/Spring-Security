735806574edd7f1eead8d675f90bc76868e730fe
Jamshid <jamshidyuldashev7799@gmail.com>
2021-09-22 12:50:42 +0500

    Add Security Config

---
+++ src/main/java/com/example/demo/model/User.java
@@ -0,0 +1,38 @@
+package com.example.demo.model;
+
+import lombok.Data;
+
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "email")
+    private String email;
+    @Column(name = "password")
+    private String password;
+    @Column(name = "first_Name")
+    private String firstName;
+    @Column(name = "last_Name")
+    private String lastName;
+    @Enumerated(value = EnumType.STRING)
+    @Column(name = "role")
+    private Role role;
+    @Enumerated(value = EnumType.STRING)
+    @Column(name = "status")
+    private Status status;
+
+
+
+
+
+
+
+
+}
